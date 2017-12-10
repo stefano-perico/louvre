bbcf6e2c066699f75d23262a70c4fc9db62168e6
Stef <stefano0012@gmail.com>
2017-12-10 20:06:03 +0100

    ajout service EstDisponible

---
+++ src/AppBundle/Service/EstDisponible.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefa
+ * Date: 10/12/2017
+ * Time: 18:34
+ */
+
+namespace AppBundle\Service;
+
+
+use Symfony\Component\HttpKernel\Tests\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\EntityRepository;
+
+class EstDisponible
+{
+    public function nombreBillet()
+    {
+
+    }
+
+}
